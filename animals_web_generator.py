49af69f2a9aac34a48fcd95d6b994b918767a5e2
Ha Tran <hatran3887@googlemail.com>
2026-05-27 11:10:25 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,23 +8,24 @@ def load_data(file_path):
 
 
 def main():
+    """Main function"""
     animals_data = load_data('animals_data.json')
     animals_data_string = ''
     for animal in animals_data:
-        animals_data_string += f'Name: {animal["name"]}\n'
-        animals_data_string += f'Diet: {animal["characteristics"]["diet"]}\n'
-        animals_data_string += f'Location: {animal["locations"][0]}\n'
+        animals_data_string += '<li class="cards__item">\n'
+        animals_data_string += f'Name: {animal["name"]}<br/>\n'
+        animals_data_string += f'Diet: {animal["characteristics"]["diet"]}<br/>\n'
+        animals_data_string += f'Location: {animal["locations"][0]}<br/>\n'
 
         animal_type = animal["characteristics"].get("type")
         if animal_type:
-            animals_data_string += f'Type: {animal_type}\n'
-        animals_data_string += '\n'
+            animals_data_string += f'Type: {animal_type}<br/>\n'
+        animals_data_string += '</li>\n'
 
     with open('animals_template.html', 'r') as handle:
         template = handle.read()
 
     filled_template = template.replace('__REPLACE_ANIMALS_INFO__', animals_data_string)
-
     with open('animals.html', 'w') as handle:
         handle.write(filled_template)
 
