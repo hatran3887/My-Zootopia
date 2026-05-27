932556bb3a482084f16ad399b38cfa6ac192e0ed
Ha Tran <hatran3887@googlemail.com>
2026-05-27 11:23:36 +0200

    move serialize animal to separate function + code style

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,4 +1,9 @@
+"""
+Animals Web Generator
+"""
 import json
+
+REPLACE_TEMPLATE_TEXT = '__REPLACE_ANIMALS_INFO__'
 
 
 def load_data(file_path):
@@ -7,27 +12,34 @@ def load_data(file_path):
         return json.load(handle)
 
 
+def serialize_animal(animal_obj):
+    """Serializes an animal object"""
+    animal_string = ''
+    animal_string += '<li class="cards__item">\n'
+    animal_string += f'<div class="card__title">{animal_obj["name"]}</div>\n'
+    animal_string += '<p class="card__text">\n'
+    animal_string += f'<strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+    animal_string += f'<strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+
+    animal_type = animal_obj["characteristics"].get("type")
+    if animal_type:
+        animal_string += f'<strong>Type:</strong> {animal_type}<br/>\n'
+    animal_string += '</p>\n'
+    animal_string += '</li>\n'
+    return animal_string
+
+
 def main():
     """Main function"""
     animals_data = load_data('animals_data.json')
     animals_data_string = ''
     for animal in animals_data:
-        animals_data_string += '<li class="cards__item">\n'
-        animals_data_string += f'<div class="card__title">{animal["name"]}</div>\n'
-        animals_data_string += '<p class="card__text">\n'
-        animals_data_string += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-        animals_data_string += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-
-        animal_type = animal["characteristics"].get("type")
-        if animal_type:
-            animals_data_string += f'<strong>Type:</strong> {animal_type}<br/>\n'
-        animals_data_string += '</p>\n'
-        animals_data_string += '</li>\n'
+        animals_data_string += serialize_animal(animal)
 
     with open('animals_template.html', 'r') as handle:
         template = handle.read()
 
-    filled_template = template.replace('__REPLACE_ANIMALS_INFO__', animals_data_string)
+    filled_template = template.replace(REPLACE_TEMPLATE_TEXT, animals_data_string)
     with open('animals.html', 'w') as handle:
         handle.write(filled_template)
 
