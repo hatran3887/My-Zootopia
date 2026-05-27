59c48290007c2fb9f440efe45312c3fa0dafc098
Ha Tran <hatran3887@googlemail.com>
2026-05-27 10:53:15 +0200

    Put animals information into a new html using animals_template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,14 +7,26 @@ def load_data(file_path):
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def main():
+    animals_data = load_data('animals_data.json')
+    animals_data_string = ''
+    for animal in animals_data:
+        animals_data_string += f'Name: {animal["name"]}\n'
+        animals_data_string += f'Diet: {animal["characteristics"]["diet"]}\n'
+        animals_data_string += f'Location: {animal["locations"][0]}\n'
 
-for animal in animals_data:
-    print(f'Name: {animal["name"]}')
-    print(f'Diet: {animal["characteristics"]["diet"]}')
-    print(f'Location: {animal["locations"][0]}')
+        animal_type = animal["characteristics"].get("type")
+        if animal_type:
+            animals_data_string += f'Type: {animal_type}\n'
+        animals_data_string += '\n'
 
-    animal_type = animal["characteristics"].get("type")
-    if animal_type:
-        print(f'Type: {animal_type}')
-    print()
+    with open('animals_template.html', 'r') as handle:
+        template = handle.read()
+
+    filled_template = template.replace('__REPLACE_ANIMALS_INFO__', animals_data_string)
+
+    with open('animals.html', 'w') as handle:
+        handle.write(filled_template)
+
+if __name__ == '__main__':
+    main()
